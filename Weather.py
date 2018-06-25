a3d3ac538a2c55dc72a3c9ac25a3a22d989828d1
jblazzy <blaz_j1@denison.edu>
2018-06-25 18:31:36 -0400

    Decided to pivot from using a webscraper with Weather.com to using the OpenWeatherMap API

--- Weather.py
+++ Weather.py
@@ -1,2 +1,21 @@
-# command line weather program
-# uses selenium to search the input zip code at weather.com and get the current weather
+"""
+-- Weather.py
+-- Josh Blaz
+-- Denison University -- 2019
+-- blaz_j1@denison.edu
+"""
+import requests
+
+def main():
+    ZIP = input("Please enter a ZIP Code: \n")
+    """
+    unsure if ZIP needs to be a string or int yet
+    for now will leave as string
+    """  
+    URL = "api.openweathermap.org/data/2.5/weather?zip={}".format(ZIP)
+    print(URL)
+
+
+
+if __name__ == "__main__":
+    main()
