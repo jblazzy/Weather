3dc73efdee5dbc5bf002c56d6095a973e0145ae9
jblazzy <blaz_j1@denison.edu>
2018-06-26 18:54:03 -0400

    General weather info complete

--- Weather.py
+++ Weather.py
@@ -12,11 +12,22 @@ def main():
     unsure if ZIP needs to be a string or int yet
     for now will leave as string
     """  
-    URL = "api.openweathermap.org/data/2.5/weather?zip={}".format(ZIP)
-    print(URL)
+    # URL for basic weather info
+    URL1 = "http://api.openweathermap.org/data/2.5/weather?appid=eb8707e702d345d487b0f91037928f2d&zip={}".format(ZIP)
+    json1 = requests.get(URL1).json()
+    weather = json1['weather']
 
-    reply = requests.get(URL)
-    print(reply)
+    # URL for the forecast
+    URL2 = "api.openweathermap.org/data/2.5/forecast?zip={}".format(ZIP)
+    json2 = requests.get(URL1).json()
+    temp = json1['main']['temp']
+    temp = (9/5) * (temp - 273) + 32
+    #° F = 9/5 (K - 273) + 32
+    print("Temperature: " + str(temp) + " °F")
+    print("Current Weather: " + weather[0]['main'])
+    print("Description: " + weather[0]['description'])
+    #print("Forecast: ")
+    # need to implement unix timestamp converter for 'dt' values
 
     # Use weather warnings from api aswell
     # There are currently no weather warnings
