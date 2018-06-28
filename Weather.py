437ccffe0cdd253acbec8005d0daac49140a7dd9
jblazzy <blaz_j1@denison.edu>
2018-06-28 18:00:04 -0400

    Finished forecast option

--- Weather.py
+++ Weather.py
@@ -6,17 +6,18 @@
 """
 import requests
 import datetime
+import calendar
 
 def UnixConverter(timestamp):
     """
     Converts Unix Timestamps to normal time.
+    See https://timestamp.online/article/how-to-convert-timestamp-to-datetime-in-python for Unix-to-time Conversion formatting.
     """
-    return(datetime.datetime.fromtimestamp(int(timestamp)).strftime('%m-%d %H:%M'))
-    # ('%Y-%m-%d %H:%M:%S') for full time string (Year, month, day)
+    return(datetime.datetime.fromtimestamp(int(timestamp)).strftime('%a, %B %dth %I%p'))
 
 def KelvinToFahrenheit(temp):
     """
-    Converts Kelvin temperature to Fahrenheit.
+    Converts temperature value from Kelvin to Fahrenheit.
     """
     return((9/5) * (temp - 273) + 32)
 
@@ -34,17 +35,28 @@ def main():
     # URL for the forecast
     URL2 = "http://api.openweathermap.org/data/2.5/forecast?appid=eb8707e702d345d487b0f91037928f2d&zip={}".format(ZIP)
     json2 = requests.get(URL2).json()
-    temp = json1['main']['temp']
+    tempstr1 = str(KelvinToFahrenheit(json1['main']['temp']))
 
+    print("Current Weather Status")
     print("City: " + json1['name'])
-    print("Temperature: " + str(KelvinToFahrenheit(temp)) + " °F")
+    print("   Temperature: " + tempstr1[0:4] + " °F")
     print("Current Weather: " + weather[0]['main'])
     print("Description: " + weather[0]['description'])
-    print("Forecast:")
-    for i in range(1, len(json2['list'])):  
-        #print(UnixConverter(json2['list'][i]['dt']))
-        print(json2['list'][i])
 
+    Forecast = input("Would you like to see the forecast? (Yes/No) \n")
+
+    if (Forecast == "Yes"):
+        print("Forecast:")
+        for i in range(1, len(json2['list'])):  
+            print(UnixConverter(json2['list'][i]['dt']))
+            tempstr2 = str(KelvinToFahrenheit(json2['list'][i]['main']['temp']))
+            print("   Temperature: " + tempstr2[0:4] + " °F")
+            print("   " + "Current Weather: " + json2['list'][i]['weather'][0]['main'])
+            print("   " + "Description: " + json2['list'][i]['weather'][0]['description'])
+            print("   " + "Humidity: " + str(json2['list'][i]['main']['humidity']) + "%")
+            print("")
+    else:
+        return
     # next step! : 
     # implement unix timestamp converter for 'dt' values in forecast data
 
